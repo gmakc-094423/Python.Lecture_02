08c304bcf0c71de3d9d377d823156847c8a714ea
gmakc-094423 <gmakc-094423@yandex.ru>
2022-07-19 19:53:06 +0300

    ДЗ 3 - готово

--- DZ_03-02.py
+++ DZ_03-02.py
@@ -5,3 +5,22 @@
 список: ["йцу", "фыв", "ячс", "цук", "йцукен"], ищем: "йцу", ответ: -1
 список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
 список: [], ищем: "123", ответ: -1 """
+
+
+test_list = []
+print(f"список: {test_list}")
+test_item = input("Введите искомую строку: ")
+
+
+def check_list(test_list, test_item):
+    count = 0
+    for i in range(len(test_list)):
+        if test_list[i] == test_item:
+            count += 1
+            if count == 2:
+                return i
+    else:
+        return -1
+
+
+print(f"ответ: {check_list(test_list, test_item)}")
